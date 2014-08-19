3e0a4ecadc5214b8219be1e105ae4d1d02342f3f
KeithLimon <ericsanchez@Limon.local>
2014-08-19 11:52:11 -0500

    teusday work

--- arithmetic.php
+++ arithmetic.php
@@ -1,23 +1,49 @@
 <?php
 
-function add($a, $b){
-	echo $a + $b;
-}
-function subtract($a, $b){
-	echo $a - $b;
-}
-function multiply($a, $b){
-	echo $a * $b;
-}
+// function add($a, $b){
+// 	if (is_numeric ($a) && is_numeric($b)) {
+// 		return $a + $b;
+// 	} else {
+// 		echo "FALSE" . PHP_EOL;
+// 	}
+// }
+
+// $Addition = add (3, 0) . PHP_EOL;
+
+
+// function subtract($a, $b){
+// 	if (is_numeric ($a) && is_numeric($b)) {
+// 		return $a - $b;
+// 	} else {
+// 		echo "FALSE" . PHP_EOL;
+// 	}
+	
+// }
+// function multiply($a, $b){
+// 	if (is_numeric($a) && is_numeric($b)) {
+// 		return $a * $b;
+// 	} else{
+// 		echo "FALSE" . PHP_EOL;
+// 	}
+
+// }
+
 function divide($a, $b){
-	echo $a / $b;
-}
-function modulus($a, $b){
-	echo $a % $b;
+	////the "&& b != 0" is to show an error for when you divide by 0
+	if (is_numeric($a) && is_numeric ($b) && b != 0){
+
+		return $a / $b;
+	} else "FALSE" . PHP_EOL;
+
 }
 
-// modulus(4, 5);
+$division = divide(3, 0)
 
-modulus(6, 8);
+// function modulus($a, $b){
+// 	if (is_numeric($a) && is_numeric($b)){
+// 		return $a % $b;
+// 	} else "FALSE" . PHP_EOL;
+// }
+
 
 ?> 
---
+++ internal_functions.php
@@ -0,0 +1,24 @@
+<?php
+// you dont use the same names given in the exapmle!!!!!
+// $item = (EMPTY);
+$item2 = 'words';
+// $array_item = array(1,2,3);
+
+
+
+// Create a function that checks if a variable is set or empty, and display "$variable_name is SET|EMPTY"
+var_dump( unserialize($item2)); 
+
+// TEST: If var $item is set, display '$item is SET'
+// //bol(true)
+
+// TEST: If var $item is empty, display '$item is EMPTY'
+// //bol(true)
+
+// TEST: If var $item2 is set, display '$item2 is SET'
+// //bol(true)
+
+// Serialize the array $array_item, and output the results
+// //bol(true)
+
+// Unserialize the array $arra_item, and output the results
